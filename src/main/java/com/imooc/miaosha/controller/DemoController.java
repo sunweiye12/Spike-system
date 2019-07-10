99fa09de13e69c617738b1428d61da097edc9f3f
sunweiye12 <794982825@qq.com>
2019-07-10 10:39:46 +0800

    12

--- src/main/java/com/imooc/miaosha/controller/DemoController.java
+++ src/main/java/com/imooc/miaosha/controller/DemoController.java
@@ -1,16 +1,24 @@
 package com.imooc.miaosha.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.result.CodeMsg;
 import com.imooc.miaosha.result.Result;
+import com.imooc.miaosha.service.UserService;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+
+		//注入server对象,调用里面的接口
+	
+		@Autowired
+		UserService userService;
 	
 	 	@RequestMapping("/")
 	    @ResponseBody
@@ -38,5 +46,14 @@ public class DemoController {
 	 		model.addAttribute("name", "Joshua");
 	 		return "hello";
 	    }
+	 	
+	 	@RequestMapping("/db/get")
+	    @ResponseBody
+	    public Result<User> dbGet() {
+	    	User user = userService.getById(1);
+	        return Result.success(user);
+	    }
+	 	
+	 	
 	 	
 }
